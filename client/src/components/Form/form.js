5b06e69d50aaf1eacd33d9de388509bb8729ff59
lee2020 <liping.ma36@gmail.com>
2022-01-10 21:56:52 -0500

    03-implement-redux

---
+++ client/src/components/Form/form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import useStyles from './form_styles';
+
+const Form = () => {
+    const classes = useStyles();
+    return (
+        <h1>FORM</h1>
+    )
+}
+
+export default Form;
